66acbc9e72116121ff84dd6d5911903ab47ec368
Headrun <sivak@headrun.net>
2017-05-19 12:30:46 +0000

    Script to run pylint on multiple python files.

--- pylint_run.py
+++ pylint_run.py
@@ -9,16 +9,16 @@ Purpose: It will run pylint for all .py files in a dir,
 from pylint.lint import Run
 import glob, os
 
-file_conn = open('pylint_scores.txt', 'w')
+FILE_CONNECTION = open('pylint_scores.txt', 'w')
 
 os.chdir('*path*')
-for file in glob.glob("*.py"):
-    results = Run([file], exit=False)
+for file_ in glob.glob("*.py"):
+    results = Run([file_], exit=False)
     try:
         score = results.linter.stats['global_note']
     except KeyError:
         score = -1
     if score and score < 0:
-        file_conn.write("{}  {}\n".format(file, score))
+        FILE_CONNECTION.write("{}  {}\n".format(file_, score))
         print file, '\t', score
-file_conn.close()
+FILE_CONNECTION.close()
